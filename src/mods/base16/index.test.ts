229203deecee3e9a9acb88cff3c36cff0c4f2d1a
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-09-09 21:16:18 +0200

    copy

--- src/mods/base16/index.test.ts
+++ src/mods/base16/index.test.ts
@@ -7,14 +7,14 @@ import { fromScure } from "./scure.js"
 test("encode and decode", async ({ message }) => {
   const scure = fromScure(base16)
   const encodeda = scure.tryEncode(new Uint8Array([1, 2, 3, 4, 5, 6, 7])).unwrap()
-  const decodeda = scure.tryPadStartAndDecode("1020304050607").unwrap().copy()
+  const decodeda = scure.tryPadStartAndDecode("1020304050607").unwrap().copyAndDispose()
 
   console.log(encodeda, decodeda)
 
   await Alocer.initBundledOnce()
   const alocer = fromAlocer(Alocer)
   const encodedb = alocer.tryEncode(new Uint8Array([1, 2, 3, 4, 5, 6, 7])).unwrap()
-  const decodedb = alocer.tryPadStartAndDecode("1020304050607").unwrap().copy()
+  const decodedb = alocer.tryPadStartAndDecode("1020304050607").unwrap().copyAndDispose()
 
   console.log(encodedb, decodedb)
 
